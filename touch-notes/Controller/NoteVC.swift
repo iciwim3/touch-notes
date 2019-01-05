8552ad0add75d3cc1b082459dd981ff51db48dc6
SKE <appybuildmore@gmail.com>
2019-01-04 18:37:05 -0800

    Created new project for note taking app and built the layout of the View in Storyboard. Made connections for tableview and conformed to protocols.

---
+++ touch-notes/Controller/NoteVC.swift
@@ -0,0 +1,44 @@
+//
+//  NoteVC.swift
+//  touch-notes
+//
+//  Created by Sain-R Edwards on 1/4/19.
+//  Copyright © 2019 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class NoteVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+
+}
+
+extension NoteVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    
+}
+
