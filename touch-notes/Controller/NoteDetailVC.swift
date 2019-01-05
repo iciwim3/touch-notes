6eb57881fa690b60f9111a3e2c08d1355ada0674
SKE <appybuildmore@gmail.com>
2019-01-04 19:38:04 -0800

    Created NoteDetailVC on Storyboard and the View Controller file. Added a text view and button. Connected them to VC file.

---
+++ touch-notes/Controller/NoteDetailVC.swift
@@ -0,0 +1,24 @@
+//
+//  NoteDetailVC.swift
+//  touch-notes
+//
+//  Created by Sain-R Edwards on 1/4/19.
+//  Copyright © 2019 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailVC: UIViewController {
+    
+    @IBOutlet weak var noteTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func lockNoteButtonPressed(_ sender: Any) {
+    }
+    
+}
