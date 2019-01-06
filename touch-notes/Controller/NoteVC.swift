dbda75434d9f5d33d2d3f1c8b34160af96a7ebe3
SKE <appybuildmore@gmail.com>
2019-01-05 21:12:17 -0800

    Added code to use Touch ID and Face ID. Also handled errors.

--- touch-notes/Controller/NoteVC.swift
+++ touch-notes/Controller/NoteVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class NoteVC: UIViewController {
     
@@ -28,7 +29,19 @@ class NoteVC: UIViewController {
     // Function that is called when we tap on a cell
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // pass note and present view controller
-        pushNoteFor(indexPath: indexPath)
+        if notesArray[indexPath.row].lockStatus == .locked {
+            authenticateBiometrics { (authenticated) in
+                if authenticated {
+                    let lockStatus = notesArray[indexPath.row].lockStatus
+                    notesArray[indexPath.row].lockStatus = lockStatusFlipper(lockStatus)
+                    DispatchQueue.main.async {
+                        self.pushNoteFor(indexPath: indexPath)
+                    }
+                }
+            }
+        } else {
+            pushNoteFor(indexPath: indexPath)
+        }
     }
     
     // Allows the 'didSelectRowAt' function to work properly in this case. We pass a note and an indexPath.
@@ -38,6 +51,41 @@ class NoteVC: UIViewController {
         noteDetailVC.index = indexPath.row
         navigationController?.pushViewController(noteDetailVC, animated: true)
     }
+    
+    func authenticateBiometrics(completion: @escaping (Bool) -> Void) {
+        let myContext = LAContext()
+        let myLocalizedReasonString = "This app uses Touch ID / Face ID to secure your notes."
+        var authError: NSError?
+        
+        if #available(iOS 8.0, macOS 10.2.1, *) {
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { (success, evaluateError) in
+                    if success {
+                        completion(true)
+                    } else {
+                        guard let evaluateErrorString = evaluateError?.localizedDescription else { return }
+                        // present alert
+                        self.showAlert(withMessage: evaluateErrorString)
+                        completion(false)
+                    }
+                }
+            } else {
+                guard let authErrorString = authError?.localizedDescription else { return }
+                self.showAlert(withMessage: authErrorString)
+                completion(false)
+            }
+        } else {
+            completion(false)
+        }
+    }
+    
+    func showAlert(withMessage message: String) {
+        let alertVC = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertVC.addAction(action)
+        present(alertVC, animated: true, completion: nil)
+    }
+    
 }
 
 extension NoteVC: UITableViewDelegate, UITableViewDataSource {
